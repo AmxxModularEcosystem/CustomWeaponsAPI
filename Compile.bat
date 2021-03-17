df0ef8c010237023fbc7ec4156d588e4dec22a14
ArKaNeMaN <arkaneman1@gmail.com>
2021-03-17 16:19:47 +0300

    Removed .bat files from .gitignore

---
+++ Compile.bat
@@ -0,0 +1,11 @@
+@echo off
+
+ECHO Compiling...
+copy /y .\include\cwapi.inc C:\AmxModX\1.9.0\include
+copy /y .\include\cwapi.inc C:\AmxModX\1.10.0\include
+amxx190 .\CustomWeaponsAPI.sma
+
+ECHO .
+ECHO .
+ECHO .
+set /p set q=Done...
